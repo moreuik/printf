9fd793268261d4705ab2626eaf6a189af765b22d
Subaia Ibraheem <subaiaibraheem@gmail.com>
2023-08-22 10:43:45 -0700

    conversions

---
+++ conversion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: always 0 when successful
+ */
+
+int main(void)
+{
+	int num1 = 50;
+	int num2 = 150;
+
+	printf("%d\n", num1);
+	printf("%i\n", num2);
+
+	return (0);
+}
